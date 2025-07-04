ae3605d756b44079d3db868083be2365b6575c01
VozSoldat <muhamaderril7@gmail.com>
2025-07-04 12:41:50 +0700

    feat: add pause and pause menu

---
+++ Assets/App/Scripts/Components/GamePause/MB_GamePause.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class MB_GamePause : MonoBehaviour
+{
+    public Action OnPauseEnter;
+    public Action OnPauseExit;
+    [SerializeField] bool useEsc = true;
+    bool isPaused = false;
+    void Start()
+    {
+        OnPauseEnter += () => { isPaused = true; Time.timeScale = 0; };
+        OnPauseExit += () => { isPaused = false; Time.timeScale = 1; };
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            OnPauseEnter?.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            OnPauseExit?.Invoke();
+        }
+    }
+}
