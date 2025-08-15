1a10162de4f3b86c59475ddb75b93d2b5bfc2efa
VozSoldat <muhamaderril7@gmail.com>
2025-08-15 21:24:33 +0700

    Improve AI movement update timing

--- Assets/App/Scripts/Components/CharacterMovements/AIMovement/MB_EnemyMovement.cs
+++ Assets/App/Scripts/Components/CharacterMovements/AIMovement/MB_EnemyMovement.cs
@@ -3,19 +3,6 @@ using UnityEngine;
 
 public class MB_EnemyMovement : MB_CharacterMovement
 {
-    // public override void FixedUpdate()
-    // {
-
-    //     // if (!_overrideStopWalk)
-    //     // {
-    //     //     _walkable.SetPosition(_characterControl.TargetDirection);
-    //     // }
-    //     // else
-    //     // {
-    //     //     _walkable.SetPosition(Vector3.zero);
-    //     // }
-    //     // Debug.Log($"Target Direction: {_characterControl.TargetDirection}");
-    // }
 
     public void UpdateInState()
     {
