d257359351bb48ed43f8b30f1d07e484eb0234df
Daffa Maulana Satria <daffamaulanasatria@gmail.com>
2025-08-19 08:21:57 +0700

    feat : puzzle template

--- Assets/App/Scripts/Components/Puzzle/MB_AbstractPuzzleBase.cs
+++ Assets/App/Scripts/Components/Puzzle/MB_AbstractPuzzleBase.cs
@@ -1,25 +1,25 @@
 using System;
+using PuzzleInteraction.Controllers;
 using UnityEngine;
 
-public abstract class MB_AbstractPuzzleBase : MonoBehaviour, IPuzzleTrigger
+namespace App.Scripts.Components.Puzzle
 {
-    [SerializeField] protected bool isPuzzleFinished = false;
-    public bool IsPuzzleFinished => isPuzzleFinished;
-
-    [SerializeField] protected string puzzleName;
-    public string PuzzleName => puzzleName;
-
-    public static event Action<MB_AbstractPuzzleBase> OnThisPuzzleFinished;
-
-    public abstract void TriggerPuzzle();
-    public abstract void ResetPuzzle();
-
-    public virtual void OnPuzzleFinished()
+    public abstract class MB_AbstractPuzzleBase : MB_AbstractController
     {
-        if (isPuzzleFinished) return;
+        [SerializeField] protected bool isPuzzleFinished = false;
+        public bool IsPuzzleFinished => isPuzzleFinished;
+    
+        public static event Action<MB_AbstractPuzzleBase> OnThisPuzzleFinished;
+    
+        public abstract void ResetPuzzle();
 
-        isPuzzleFinished = true;
-        Debug.Log("Puzzle finished!");
-        OnThisPuzzleFinished?.Invoke(this);
+        public virtual void OnPuzzleFinished()
+        {
+            if (isPuzzleFinished) return;
+
+            isPuzzleFinished = true;
+            Debug.Log("Puzzle finished!");
+            OnThisPuzzleFinished?.Invoke(this);
+        }
     }
 }
